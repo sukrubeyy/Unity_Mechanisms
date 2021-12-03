6815493c30c8a9ae56cbf02afee18b84c5e2dfe7
sukrubeyy <sukru.beyy@outlook.com>
2021-12-03 23:06:08 +0300

    fall damage script changed

---
+++ Fall_Damage/Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public IEnumerator Shaker(float time, float force)
+    {
+        Vector3 originalTransform = transform.localPosition;
+        float elapsed = 0;
+        while (elapsed < time)
+        {
+            float x = Random.Range(-1f, 1f) * force;
+            float y = Random.Range(-1f, 1f) * force;
+            transform.localPosition = new Vector3(x, y, originalTransform.z);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = originalTransform;
+    }
+}
---
+++ Fall_Damage/Assets/Scripts/FallDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class FallDamage : MonoBehaviour
+{
+    public Vector3 veloctiy;
+    public Rigidbody rb;
+    public CameraShaker cameraShakerScript;
+    float damage;
+    public float jumpForce;
+    void Start()
+    {
+        jumpForce = 320f;
+        cameraShakerScript = transform.GetComponentInChildren<CameraShaker>();
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0,jumpForce,0));
+           
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        damage =  collision.relativeVelocity.magnitude;
+        Debug.Log(damage/100);
+        if(damage>0)
+            StartCoroutine(cameraShakerScript.Shaker(.15f,damage/100));
+    }
+}
