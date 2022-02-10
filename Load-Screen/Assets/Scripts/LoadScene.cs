5e3608f89338d155af4f92ed207ff799fa89672b
sukrubeyy <sukru.beyy@outlook.com>
2022-02-11 01:00:18 +0300

    Load Screen Added

---
+++ Load-Screen/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject parentSlider;
+
+    public void LoadLevel(int IndexLevel)
+    {
+        StartCoroutine(AsyncLoadLevel(IndexLevel));
+    }
+
+    IEnumerator AsyncLoadLevel(int IndexLevel)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(IndexLevel);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
+        
+    }
+}
